b950133258376c46f5ddc13ffba4ee120466eb09
Santiago <sgobotta@gmail.com>
2019-06-29 10:50:07 -0300

    Tests

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@ INPUT_SIZE=$1
 FILE_NAME=''
 OUTPUT_FILE="$(pwd)/output"
 
-if [ $1 = 'large' ]
+if [ $1 = 'small' ]
 then
   FILE_NAME='A-small-practice.in'
 else
   FILE_NAME='A-large-practice.in'
+fi
+
+if [ $1 = 'sample' ]
+then
+  FILE_NAME='sample.in'
 fi
 
 FILE="$(pwd)/input/$FILE_NAME"
--- solution.hs
+++ solution.hs
@@ -1,6 +1,7 @@
 import Data.List.Split
+import System.Environment
 
-data Cell = X | O | D | T deriving (Show)
+data Cell = X | O | D | T deriving (Show, Eq)
 
 toCell :: Char -> Cell
 toCell '.' = D
@@ -8,7 +9,34 @@ toCell 'X' = X
 toCell 'O' = O
 toCell 'T' = T
 
-data Case = WX | WO | Draw | Unfinished
+data Case = WX | WO | Draw | Unfinished deriving (Show, Eq)
+
+eval :: [Cell] -> Case
+eval cs = if any (\c -> c == D) cs
+          then Unfinished
+          else if isWinner X cs
+               then WX
+               else if isWinner O cs
+                    then WO
+                    else Draw
+
+evalCase :: [Case] -> Case
+evalCase cs = if any (\c -> c == WX) cs
+              then WX
+              else if any (\c -> c == WO) cs
+                   then WO
+                   else if any (\c -> c == Unfinished) cs
+                        then Unfinished
+                        else Draw
+
+answer :: Case -> String
+answer WX         = "X won"
+answer WO         = "O won"
+answer Draw       = "Draw"
+answer Unfinished = "Game has not completed"
+
+isWinner :: Cell -> [Cell] -> Bool
+isWinner c cs = all (\x -> x == c || x == T) cs
 
 parseCases :: String -> [[[Cell]]]
 parseCases x = let (n:ts) = filter (not . null) (lines x)
@@ -28,7 +56,7 @@ getRows [] = []
 getRows (x:xs) = (toCellList x) : getRows xs
 
 toCellList :: String -> [Cell]
-toCellList s = map toCell s
+toCellList = map toCell
 
 getColumns :: [String] -> Int -> [[Cell]]
 getColumns ts l = map (\n -> getColumn n ts) [0..l]
@@ -45,8 +73,22 @@ getDiagonals xs l = let makeIndexOf = (\(ss,n) xs -> toCell (ss !! n) : xs)
                     in
                       (getDiagonal id) : (getDiagonal reverse) : []
 
+showC :: (Int, String) -> String
+showC (n, s) = "Case #" ++ show n ++ ": " ++ s
+
 main = do
-  ts  <- parseCases `fmap` getContents
-  flip mapM_ (zip [1..] ts) $ \(i,t) -> do
-  putStrLn $ "Case #" ++ show i ++ ": " ++ show t
+  args <- getArgs
+  cases  <- parseCases `fmap` (readFile (head args)) -> do
+  putStr (map show cases)
   return ()
+
+
+  -- flip mapM_ (zip [1..] cases) $ \(i,t) -> do
+  --   putStrLn $ "Case #" ++ show i ++ ": " ++ answer (evalCase (map eval t))
+
+  -- main = do
+  --   args <- getArgs
+  --   cases  <- parseCases `fmap` (readFile (head args))
+  --   sols <- map (\c -> answer (evalCase (map eval c))) cases
+  --   writeFile (unlines map showC (zip [1..] sols)) "small"
+  --   return ()

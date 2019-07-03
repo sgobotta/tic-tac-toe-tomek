a6632e5d979ab252e8901ec42dbba8b77fd55b0b
Tehuel <torresbaldi.tehuel@gmail.com>
2019-07-03 20:30:52 -0300

    adds new specific types and adjust comments

--- solution.hs
+++ solution.hs
@@ -23,18 +23,22 @@ toCell 'T' = T
 -- Unfinished: the game did not finished
 data Case = WX | WO | Draw | Unfinished deriving (Show, Eq)
 
+-- A row is used to represent a row in a board, but also represents a column or a diagnoal of the board. 
+type Odd = [Cell]
 type Row = [Cell]
-type Board = [Row]
+type Column = [Cell]
+type Diagonal = [Cell]
 
--- Purpose: given a list of lists of Cell, representing every odd in a
--- tic-tac-toe-tomek game, returns a simplified result for a single
--- tic-tac-toe-tomek case.
+-- A board type is a list of all the rows, columns and diagonals in a given board case. Every odd.
+type Board = [[Cell]]
+
+-- Purpose: given a Board, representing every odd in a tic-tac-toe-tomek game, 
+-- returns a simplified result for a single tic-tac-toe-tomek case.
 evalCaseChunks :: Board -> [Case]
 evalCaseChunks css = map evalCaseChunk css
 
--- Purpose: given a list of Cell, representing an odd of a tic-tac-toe-tomek
--- game, returns the state of that odd represented as a Case.
-evalCaseChunk :: Row -> Case
+-- Purpose: given an Odd (Row or Column or Diagonal),  returns the state of that odd represented as a Case.
+evalCaseChunk :: Odd -> Case
 evalCaseChunk cs = if any (\c -> c == D) cs
                    then Unfinished
                    else if isWinner X cs
@@ -43,9 +47,8 @@ evalCaseChunk cs = if any (\c -> c == D) cs
                              then WO
                              else Draw
 
--- Purpose: given a Cell `c` (X or O), evaluates a list of Cell to determine
--- if `c` is a winner.
-isWinner :: Cell -> Row -> Bool
+-- Purpose: given a Cell `c` (X or O), evaluates an Odd to determine if `c` is a winner.
+isWinner :: Cell -> Odd -> Bool
 isWinner c = all (\x -> x == c || x == T)
 
 -- Purpose: given a list of Case, representing a bunch of odds for a single
@@ -59,6 +62,7 @@ evalCases cs = if any (\c -> c == WX) cs
                     else if any (\c -> c == Unfinished) cs
                          then Unfinished
                          else Draw
+
 -- Purpose: given a Case, returns a String representing a game result.
 answer :: Case -> String
 answer WX         = "X won"
@@ -67,58 +71,57 @@ answer Draw       = "Draw"
 answer Unfinished = "Game has not completed"
 
 
--- Purpose: given a file content with `n` cases, returns a list of `n` cases
--- where each case is represented as a list of Cell lists. Each list of Cell
+-- Purpose: given a file content with `n` cases, returns a list of `n` Boards,
+-- where each board is represented as a list of rows. Each of these rows
 -- represents an odd for a tic-tac-toe-tomek row, column and diagonal odds.
 --
 -- Precondition: in the file content input, each case must be separated with a
 -- line break.
-parseCases :: String -> [Board]
-parseCases x = let (n:ts) = filter (not . null) (lines x)
+parseContent :: String -> [Board]
+parseContent x = let (n:ts) = filter (not . null) (lines x)
                in
-                 take (read n) (parseCase (chunksOf 4 ts))
+                 take (read n) (parseCases (chunksOf 4 ts))
 
 -- Purpose: given a case represented as a list of String, returns all possible
 -- odds in a tic-tac-toe-tomek game where each character is represented as a
 -- Cell.
-parseCase :: [[String]] -> [Board]
-parseCase []       = []
-parseCase (xs:xss) = (generateOdds xs) : parseCase xss
+parseCases :: [[String]] -> [Board]
+parseCases []       = []
+parseCases (xs:xss) = (generateOdds xs) : parseCases xss
 
 -- Purpose: given a list of String representing tic-tac-toe-tomek rows,
--- generates every odd of a game to return a list of lists of Cell, representing
+-- generates every odd of a game to return a board, representing
 -- every possible row, column and diagonal.
-generateOdds :: [String] -> Board
+generateOdds :: [String] -> [Odd]
 generateOdds ss = getRows ss
                   ++ getColumns ss ((length ss) - 1)
                   ++ getDiagonals ss ((length ss) - 1)
 
 -- Purpose: given a list of String representing tic-tac-toe-tomek rows, returns
--- a tic-tac-toe-tomek game represented as rows of Cell.
-getRows :: [String] -> Board
+-- a tic-tac-toe-tomek game represented as a board of rows.
+getRows :: [String] -> [Row]
 getRows []     = []
-getRows (x:xs) = (toCellList x) : getRows xs
+getRows (x:xs) = (getRow x) : getRows xs
 
 -- Purpose: given a String representing an odd of a tic-tac-toe-tomek game,
--- returns a list of converted characters to Cell.
-toCellList :: String -> Row
-toCellList = map toCell
+-- returns a row.
+getRow :: String -> Row
+getRow = map toCell
 
 -- Purpose: given a list of String representing tic-tac-toe-tomek rows, and an
--- Int representing the length of a game row, returns a list of lists of Cell
--- representing a game columns.
-getColumns :: [String] -> Int -> Board
+-- Int representing the length of a game row, returns a list of every game columns
+getColumns :: [String] -> Int -> [Column]
 getColumns ts l = map (\n -> getColumn n ts) [0..l]
 
 -- Purpose: given an Int representing the index of a game row, and a game row,
--- returns a list of Cell representing a game column.
-getColumn :: Int -> [String] -> Row
+-- returns a specific game column
+getColumn :: Int -> [String] -> Column
 getColumn n = map (toCell . (\s -> (!!) s n))
 
 -- Purpose: given a list of String representing a game row, and an Int
--- representing the length of a game column, returns the two columns of a
--- tic-tac-toe-tomek game.
-getDiagonals :: [String] -> Int -> Board
+-- representing the length of a game diagonal, returns the two diagonals of a
+-- tic-tac-toe-tomek game
+getDiagonals :: [String] -> Int -> [Diagonal]
 getDiagonals xs l = let makeIndexOf = (\(ss,n) xs -> toCell (ss !! n) : xs)
                         foldTTTT    = foldr makeIndexOf []
                         indexes     = [0..l]
@@ -137,7 +140,7 @@ showCase (n, s) = "Case #" ++ show n ++ ": " ++ s
 -- each case.
 main = do
   [input, output] <- getArgs
-  cases           <- parseCases `fmap` (readFile input)
+  cases           <- parseContent `fmap` (readFile input)
   writeFile output $
     unlines $
     map showCase $ zip [1..] $
